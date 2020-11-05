c70b7594234515be119bc6c87bc041938d765189
Rudnev Danil <danil.160398@yandex.ru>
2020-11-05 17:54:48 +0300

    lab 4 front close to done

---
+++ Lab_4/chroma/chroma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chroma
+//
+//  Created by Admin on 05/11/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
