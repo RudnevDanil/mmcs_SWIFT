c95be399e6bb99f4cbba179411f0007c676ec83e
Rudnev Danil <danil.160398@yandex.ru>
2020-11-07 16:57:39 +0300

    lab4 connections done

--- Lab_4/chroma/chroma/ViewController.swift
+++ Lab_4/chroma/chroma/ViewController.swift
@@ -15,6 +15,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var redColor: UISlider!
+    @IBOutlet weak var greenColor: UISlider!
+    @IBOutlet weak var blueColor: UISlider!
+    @IBOutlet weak var colorLabel: RoundButton!
+    
+    @IBAction func coloralueChanged(_ sender: Any)
+    {
+        colorLabel.backgroundColor =  UIColor(red: (CGFloat(redColor.value/255.0)), green: (CGFloat(greenColor.value/255.0)), blue: (CGFloat(blueColor.value/255.0)), alpha: 1.0)
+    }
+    
+    @IBAction func buttonLoadTouched(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func ButtonEraserTouched(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func buttonSaveTouched(_ sender: Any)
+    {
+        
+    }
+    
+    
+    
 }
 
