a82b2a326a37933e1cc688cfcadb394eb97b9202
Rudnev Danil <danil.160398@yandex.ru>
2020-11-10 15:12:09 +0300

    lab4 undo done

--- Lab_4/chroma/chroma/ViewController.swift
+++ Lab_4/chroma/chroma/ViewController.swift
@@ -8,14 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate
+{
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        //view = imageView!
-        //view = UIImageView(frame: view.bounds)
-        //view.backgroundColor = UIColor.white
         view.isUserInteractionEnabled = true
         
         let press = UILongPressGestureRecognizer(target: self, action: #selector(longPress))
@@ -43,10 +40,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var greenColor: UISlider!
     @IBOutlet weak var blueColor: UISlider!
     @IBOutlet weak var colorLabel: RoundButton!
-    var imageViewLines: UIImageView {
-        return imageView!
-        //return view as! UIImageView
-    }
+    var imageViewLines: UIImageView?
+    var originalImage : UIImage? = nil
     
     @IBAction func coloralueChanged(_ sender: Any)
     {
@@ -70,7 +65,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         {
+            originalImage = image
             imageView.image = image
+            imageViewLines = imageView
         }
         else
         {
@@ -95,9 +92,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     @IBAction func buttonUndoTouched(_ sender: Any)
     {
+        print(linesArray.count)
         if linesArray.count > 0
         {
+            print(linesArray.count)
             _ = linesArray.popLast()
+            print(linesArray.count)
+            drawLine()
         }
     }    
     
@@ -111,62 +112,70 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         taped = true
         if let touch = touches.first {
-            //lastPoint = touch.location(in: view)
-            lastPoint = touch.location(in: imageViewLines)
+            lastPoint = touch.location(in: imageView)
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         taped = false
         if let touch = touches.first {
-            //let currentPoint = touch.location(in: view)
-            let currentPoint = touch.location(in: imageViewLines)
-            drawLine(from: lastPoint, to: currentPoint)
+            let currentPoint = touch.location(in: imageView)
+            addLine(from: lastPoint, to: currentPoint)
             lastPoint = currentPoint
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if taped {
-            drawLine(from: lastPoint, to: lastPoint)
+            addLine(from: lastPoint, to: lastPoint)
         }
     }
     
     @objc func longPress() {
-        let activity = UIActivityViewController(activityItems: [imageViewLines.image as Any], applicationActivities: nil)
+        let activity = UIActivityViewController(activityItems: [imageView.image as Any], applicationActivities: nil)
         present(activity, animated:true, completion:nil)
     }
     
-    func drawLine(from fromPoint: CGPoint, to toPoint:CGPoint)
+    func addLine(from fromPoint: CGPoint, to toPoint:CGPoint)
     {
-        linesArray.append(lineInfo(startPoint: fromPoint, endPoint: toPoint, color: strokeColor))
-        
-        let stPoint = CGPoint(
-            x: (imageViewLines.frame.origin.x),
-            y: (imageViewLines.frame.origin.y))
-        
-        let imSize = CGSize(
-            width: imageViewLines.frame.size.width + imageViewLines.frame.size.width - floor(imageViewLines.frame.size.width),
-            height: imageViewLines.frame.size.height + imageViewLines.frame.size.height - floor(imageViewLines.frame.size.height))
-        
-        for curentLine in linesArray
+        if(originalImage != nil && imageViewLines != nil)
         {
-            UIGraphicsBeginImageContext(imageViewLines.frame.size)
+            linesArray.append(lineInfo(startPoint: fromPoint, endPoint: toPoint, color: strokeColor))
+            drawLine()
+        }
+    }
+    
+    func drawLine()
+    {
+        if(originalImage != nil && imageViewLines != nil)
+        {
+            imageViewLines!.image = originalImage!
+            let stPoint = CGPoint(
+                x: (imageViewLines!.frame.origin.x),
+                y: (imageViewLines!.frame.origin.y))
             
-            imageViewLines.image?.draw(in: CGRect(origin: stPoint, size: imSize))
+            let imSize = CGSize(
+                width: imageViewLines!.frame.size.width + imageViewLines!.frame.size.width - floor(imageViewLines!.frame.size.width),
+                height: imageViewLines!.frame.size.height + imageViewLines!.frame.size.height - floor(imageViewLines!.frame.size.height))
             
-            let linePath = UIBezierPath()
-            linePath.move(to: curentLine.startPoint)
-            linePath.addLine(to: curentLine.endPoint)
-            strokeColor = curentLine.color
-            strokeColor.setStroke()
-            linePath.lineWidth = strokeWidth
-            linePath.lineCapStyle = .round
-            linePath.lineJoinStyle = .round
-            linePath.stroke()
-            imageViewLines.image = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
+            for curentLine in linesArray
+            {
+                UIGraphicsBeginImageContext(imageViewLines!.frame.size)
+                
+                imageViewLines!.image?.draw(in: CGRect(origin: stPoint, size: imSize))
+                
+                let linePath = UIBezierPath()
+                linePath.move(to: curentLine.startPoint)
+                linePath.addLine(to: curentLine.endPoint)
+                strokeColor = curentLine.color
+                strokeColor.setStroke()
+                linePath.lineWidth = strokeWidth
+                linePath.lineCapStyle = .round
+                linePath.lineJoinStyle = .round
+                linePath.stroke()
+                imageViewLines!.image = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+            }
         }
     }
 }
-
